f23b302167ae3db195d9db1d76b4c163dcf01929
niju646 <nijusajeevnj@gmail.com>
2025-11-12 14:40:33 +0530

    bugfix(ui): updated the student list screen ui

--- lib/shared/widgets/profile_tile.dart
+++ lib/shared/widgets/profile_tile.dart
@@ -23,78 +23,81 @@ class ProfileTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 4),
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-        decoration: BoxDecoration(
-          border: Border.all(color: Color(0xFFCCCCCC)),
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Row(
-          children: [
-            SizedBox(
-              width: 48,
-              height: 48,
-              child: ClipOval(
-                child: CachedNetworkImage(
-                  imageUrl: imageUrl ?? "",
-                  placeholder:
-                      (context, url) => Container(
-                        color: const Color.fromARGB(255, 228, 225, 225),
-                      ),
-                  errorWidget:
-                      (context, url, error) => const Icon(
-                        Icons.person_2_rounded,
-                        color: Colors.grey,
-                      ),
-                  fit: BoxFit.cover,
+      child: GestureDetector(
+        onTap: onPressed,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+          decoration: BoxDecoration(
+            border: Border.all(color: Color(0xFFCCCCCC)),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Row(
+            children: [
+              SizedBox(
+                width: 48,
+                height: 48,
+                child: ClipOval(
+                  child: CachedNetworkImage(
+                    imageUrl: imageUrl ?? "",
+                    placeholder:
+                        (context, url) => Container(
+                          color: const Color.fromARGB(255, 228, 225, 225),
+                        ),
+                    errorWidget:
+                        (context, url, error) => const Icon(
+                          Icons.person_2_rounded,
+                          color: Colors.grey,
+                        ),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(width: 10),
-            Expanded(
-              flex: 3,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Text(
-                      capitalizeEachWord(name),
+              const SizedBox(width: 10),
+              Expanded(
+                flex: 3,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Text(
+                        capitalizeEachWord(name),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      capitalizeEachWord(description),
                       overflow: TextOverflow.ellipsis,
                       maxLines: 1,
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                        fontSize: 14,
+                        color: const Color(0xFF7C7C7C),
                         fontWeight: FontWeight.w500,
-                        fontSize: 16,
                       ),
                     ),
-                  ),
-                  Text(
-                    capitalizeEachWord(description),
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                      fontSize: 14,
-                      color: const Color(0xFF7C7C7C),
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const Spacer(),
-            TextButton(
-              onPressed: onPressed,
-              child: Text(
-                suffixText,
-                style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                  color: Colors.black,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
+                  ],
                 ),
               ),
-            ),
-          ],
+              const Spacer(),
+              TextButton(
+                onPressed: onPressed,
+                child: Text(
+                  suffixText,
+                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
